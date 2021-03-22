f1ac731343c0447bb54d0f71448727d55c4b32c0
Rozakulhalim <rojakull@gmail.com>
2021-03-22 08:53:27 +0700

    Uploaf Githublimz

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,26 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.MirrorCommand = 'mir'
+        self.UnzipMirrorCommand = 'unzip'
+        self.TarMirrorCommand = 'tarmir'
+        self.CancelMirror = 'cancel'
+        self.CancelAllCommand = 'cancelall'
+        self.ListCommand = 'list'
+        self.StatusCommand = 'status'
+        self.AuthorizedUsersCommand = 'users'
+        self.AuthorizeCommand = 'auth'
+        self.UnAuthorizeCommand = 'unauth'
+        self.AddSudoCommand = 'addsudo'
+        self.RmSudoCommand = 'rmsudo'
+        self.PingCommand = 'ping'
+        self.RestartCommand = 'restart'
+        self.StatsCommand = 'stats'
+        self.HelpCommand = 'help'
+        self.LogCommand = 'log'
+        self.CloneCommand = "clone"
+        self.WatchCommand = 'yt'
+        self.TarWatchCommand = 'taryt'
+        self.deleteCommand = 'del'
+
+BotCommands = _BotCommands()
